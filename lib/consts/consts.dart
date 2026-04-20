c982a7032d1774064f724c83baed6286b040356a
Aakash Prajapati <aakashprajapati@Aakashs-MacBook-Pro.local>
2026-04-20 13:51:22 +0530

    add category details screen

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -2,9 +2,9 @@ export './colors.dart';
 export './images.dart';
 export './strings.dart';
 export './styles.dart';
-export 'package:velocity_x/velocity_x.dart';
 export './lists.dart';
 
 // After adding this here , so we don't need to cal in every file
+export 'package:velocity_x/velocity_x.dart';
 export 'package:flutter/material.dart';
 export 'package:get/get.dart';
--- lib/views/category_screen/category_details.dart
+++ lib/views/category_screen/category_details.dart
@@ -10,6 +10,79 @@ class CategoryDetails extends StatelessWidget {
     return bgWidget(
       child: Scaffold(
         appBar: AppBar(title: title!.text.fontFamily(bold).white.make()),
+        body: Container(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                    6,
+                    (index) => "Baby Cloathing".text
+                        .size(12)
+                        .fontFamily(semibold)
+                        .color(darkFontGrey)
+                        .makeCentered()
+                        .box
+                        .white
+                        .roundedSM
+                        .size(120, 60)
+                        .margin(const EdgeInsets.symmetric(horizontal: 4))
+                        .make(),
+                  ),
+                ),
+              ),
+
+              20.heightBox,
+
+              Expanded(
+                child: Container(
+                  // color: lightGrey,
+                  child: GridView.builder(
+                    physics: BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: 6,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisExtent: 250,
+                      mainAxisSpacing: 8,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                imgP5,
+                                width: 200,
+                                height: 150,
+                                fit: BoxFit.cover,
+                              ),
+                              "Laptop 4GB/64GB".text
+                                  .fontFamily(semibold)
+                                  .color(darkFontGrey)
+                                  .make(),
+                              10.heightBox,
+                              "\$600".text
+                                  .color(redColor)
+                                  .fontFamily(bold)
+                                  .size(16)
+                                  .make(),
+                            ],
+                          ).box.white
+                          .margin(EdgeInsets.symmetric(horizontal: 5))
+                          .roundedSM
+                          .outerShadowSm
+                          .padding(EdgeInsets.all(12))
+                          .make();
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
